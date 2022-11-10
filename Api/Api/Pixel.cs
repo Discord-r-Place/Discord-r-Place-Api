e9226c42ddcf1d8275d7eb0e41790636b4d10f1f
J.Teeuwissen <jelleteeuwissen@hotmail.nl>
2022-11-10 20:30:29 +0100

    No to/from string

--- Api/Api/Pixel.cs
+++ Api/Api/Pixel.cs
@@ -7,11 +7,7 @@ public struct Pixel
     public byte Color { get; set; }
 
     public byte[] GetBytes() => new[] { (byte)(X >> 8), (byte)(X & 0xff), (byte)(Y >> 8), (byte)(Y & 0xff), Color };
-    public string GetString() => Convert.ToBase64String(GetBytes());
 
     public static Pixel FromBytes(byte[] bytes) =>
         new () { X = (ushort)((bytes[0] << 8) + bytes[1]), Y = (ushort)((bytes[2] << 8) + bytes[3]), Color = bytes[4] };
-
-    public static Pixel FromString(string s) => FromBytes(Convert.FromBase64String(s));
-     
 }
