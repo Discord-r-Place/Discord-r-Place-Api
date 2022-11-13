aed0308481b9bfc62ef35dff5edb5f0ded4529d8
J.Teeuwissen <jelleteeuwissen@hotmail.nl>
2022-11-13 13:01:16 +0100

    Receive token from websocket itself

--- Api/Api/WebSocketReceiver.cs
+++ Api/Api/WebSocketReceiver.cs
@@ -11,6 +11,28 @@ public class WebSocketReceiver
         _webSocket = webSocket;
     }
 
+    public async Task<string> ReceiveToken(CancellationToken token)
+    {
+        ArraySegment<byte> buffer = new(new byte[100]);
+        WebSocketReceiveResult? result;
+
+        using MemoryStream ms = new();
+
+        do
+        {
+            result = await _webSocket.ReceiveAsync(buffer, token);
+            ms.Write(buffer.Array!, buffer.Offset, result.Count);
+        } while (!result.EndOfMessage);
+
+        ms.Seek(0, SeekOrigin.Begin);
+
+        if (result.MessageType != WebSocketMessageType.Text)
+            throw new Exception("only text messages are supported for authentication.");
+
+        byte[] array = ms.ToArray();
+        return System.Text.Encoding.UTF8.GetString(array);
+    }
+
     public async Task<Pixel> ReceivePixel(CancellationToken token)
     {
         ArraySegment<byte> buffer = new(new byte[10]);
